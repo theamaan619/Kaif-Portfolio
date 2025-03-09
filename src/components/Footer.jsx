74ad40cf476600e49768437dcd804ecb4ffc23e2
Amaan63 <amaansayed63@gmail.com>
2025-03-09 15:06:17 +0530

    Second Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-indigo-800 text-white py-8">
+      <footer className="bg-indigo-800 text-white py-8 ">
         <div className="container mx-auto px-6 text-center">
           <p>
             © 2025 Kaif Sankhala, Society Admin & Accounting Professional. All
